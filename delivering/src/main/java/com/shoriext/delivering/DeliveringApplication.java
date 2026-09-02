42a744e290d53f0b4f9e69965a0211dc2e7ec7ae
Shoriext <urapol18@gmail.com>
2026-09-02 22:30:41 +0300

    add: new project

---
+++ delivering/src/main/java/com/shoriext/delivering/DeliveringApplication.java
@@ -0,0 +1,13 @@
+package com.shoriext.delivering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveringApplication.class, args);
+	}
+
+}
