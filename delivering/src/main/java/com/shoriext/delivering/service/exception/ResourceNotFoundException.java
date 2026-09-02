755faac686b43805158dbf7d61eff2efa8047463
Shoriext <urapol18@gmail.com>
2026-09-02 23:29:09 +0300

    исключения

---
+++ delivering/src/main/java/com/shoriext/delivering/service/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.shoriext.delivering.service.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
